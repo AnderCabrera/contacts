7e3a826c7942a53b14262b779342fb2c59163947
Ander Cabrera <andercabrerac@gmail.com>
2024-02-12 00:49:42 -0600

    adding update-modal constants

--- src/js/todo/constants.js
+++ src/js/todo/constants.js
@@ -6,3 +6,11 @@ export const titleInput = document.getElementById('title');
 export const descriptionInput = document.getElementById('description');
 export const importanceInput = document.getElementById('importance');
 export const addTodoButton = document.getElementById('add-todo');
+
+// Edit todo form constants
+export const editTodoForm = document.getElementById('edit-todo-form');
+export const editTitleInput = document.getElementById('edit-title');
+export const editDescriptionInput = document.getElementById('edit-description');
+export const editImportanceInput = document.getElementById('edit-importance');
+export const editCompletedInput = document.getElementById('edit-completed');
+export const editTodoButton = document.getElementById('edit-todo');
