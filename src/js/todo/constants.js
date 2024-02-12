634a18c8a8577696666598de49027a74b2d10393
Ander Cabrera <andercabrerac@gmail.com>
2024-02-11 23:48:51 -0600

    todo form constants added

--- src/js/todo/constants.js
+++ src/js/todo/constants.js
@@ -1,1 +1,8 @@
 export const todosTable = document.getElementById('todos');
+
+// Add a new todo form constants
+export const addTodoForm = document.getElementById('todo-form');
+export const titleInput = document.getElementById('title');
+export const descriptionInput = document.getElementById('description');
+export const importanceInput = document.getElementById('importance');
+export const addTodoButton = document.getElementById('add-todo');
